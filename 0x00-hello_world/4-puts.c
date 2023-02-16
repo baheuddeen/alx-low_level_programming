fbd28f07b1221c77b72a88f44db5d5023f0ce947
muhammad.baheuddeen@gmail.com <baheuddeen>
2023-02-16 02:22:20 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * A C program that prints to standard output using puts
+ */
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
