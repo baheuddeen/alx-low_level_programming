11c42b571959d1bedbce60140b3a238653fff5ad
muhammad.baheuddeen@gmail.com <baheuddeen>
2023-02-21 10:58:49 -0800

    WOW

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,25 +7,57 @@
  * Return: 0 (success)
 */
 
+
 int main(void)
 {
 	int count;
-	unsigned long previous = 0, current = 1;
+	char previous[30];
+	char current[30];
+	char sum[30];
+	int i = 0;
+	int carry = 0;
 
-	for (count = 0; count < 98; ++count)
+	/** set initial values */
+	for(i = 29; i >= 0; i--)
 	{
-		unsigned long temp;
-		
-		temp = current;
-		current += previous;
-		previous = temp;
-		printf("%lu", current);
+		previous[i] = current[i] = sum[i] = '0';
+	}
 
-		if (count == 97)
+    previous[29] = '0';
+    current[29] = '1';
+
+    for(count = 0; count < 98; count++)
+    {
+        /** sum = previous + current */
+        for(i = 29; i >= 0; i--)
+        {
+                sum[i] = ((previous[i] - '0') + (current[i] - '0')) + carry;
+                carry = sum[i] / 10;
+                sum[i] = sum[i] % 10 + '0';
+        }
+
+        /** print Sum */
+        bool print_flage = false;
+        for(i = 0; i <= 29; i++)
+        {
+            if (sum[i] != '0')
+                print_flage = true;
+            if(print_flage)
+                printf("%c", sum[i]);
+        }
+
+        /** copy current to previous and sum to current */
+         for(i = 29; i >= 0; i--)
+        {
+                previous[i] = current[i];
+                current[i] = sum[i];
+        }
+
+        if (count == 97)
 			printf("\n");
 		else
 			printf(", ");
-	}
 
+    }
 	return (0);
 }
