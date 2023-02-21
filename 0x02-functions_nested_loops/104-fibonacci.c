af686498c1592b9904d70c3e40105ace3a37e4f1
Muhammad Baheuddeen <84409851+baheuddeen@users.noreply.github.com>
2023-02-21 22:58:04 +0200

    Update 104-fibonacci.c
    Wow

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,64 +1,51 @@
 #include <stdio.h>
 
-
 /**
  * main - print fibonacci numbers
  *
  * Return: 0 (success)
-*/
-
+ */
 
 int main(void)
 {
-	int count;
+	int count, i = 0, carry = 0;
 	char previous[30];
 	char current[30];
 	char sum[30];
-	int i = 0;
-	int carry = 0;
 
-	/** set initial values */
-	for(i = 29; i >= 0; i--)
+	for (i = 29; i >= 0; i--)
 	{
 		previous[i] = current[i] = sum[i] = '0';
 	}
+	previous[29] = '0';
+	current[29] = '1';
+	for (count = 0; count < 98; count++)
+	{
+		int print_flage = 0;
 
-    previous[29] = '0';
-    current[29] = '1';
-
-    for(count = 0; count < 98; count++)
-    {
-	int print_flage = 0;
-
-	 /** sum = previous + current */
-        for(i = 29; i >= 0; i--)
-        {
-                sum[i] = ((previous[i] - '0') + (current[i] - '0')) + carry;
-                carry = sum[i] / 10;
-                sum[i] = sum[i] % 10 + '0';
-        }
-
-        /** print Sum */
-        for(i = 0; i <= 29; i++)
-        {
-            if (sum[i] != '0')
-                print_flage = 1;
-            if(print_flage == 1)
-                printf("%c", sum[i]);
-        }
-
-        /** copy current to previous and sum to current */
-         for(i = 29; i >= 0; i--)
-        {
-                previous[i] = current[i];
-                current[i] = sum[i];
-        }
-
-        if (count == 97)
+		/** sum = previous + current */
+		for (i = 29; i >= 0; i--)
+		{
+			sum[i] = ((previous[i] - '0') + (current[i] - '0')) + carry;
+			carry = sum[i] / 10;
+			sum[i] = sum[i] % 10 + '0';
+		}
+		for (i = 0; i <= 29; i++)
+		{
+			if (sum[i] != '0')
+				print_flage = 1;
+			if (print_flage == 1)
+				printf("%c", sum[i]);
+		}
+		for (i = 29; i >= 0; i--)
+		{
+			previous[i] = current[i];
+			current[i] = sum[i];
+		}
+		if (count == 97)
 			printf("\n");
 		else
 			printf(", ");
-
-    }
+	}
 	return (0);
 }
