07d040229f132ec01d3b96381fade13ac7599631
muhammad.baheuddeen@gmail.com <baheuddeen>
2023-02-21 07:38:03 -0800

    advanced 3

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print fibonacci numbers
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+	long previous, current;
+
+	previous = 1;
+	current = 2;
+	printf("%ld, %ld, ", previous, current);
+	for (i = 0; i < 50; i++)
+	{
+		int temp = current;
+
+		current += previous;
+		previous = temp;
+		printf("%ld", current);
+		if (i != 49)
+			printf(", ");
+	}
+	putchar('\n');
+	return (0);
+}
