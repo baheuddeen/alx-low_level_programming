8769cd0fd289e17e6aa30bb29784da1d6b941451
muhammad.baheuddeen@gmail.com <baheuddeen>
2023-02-21 09:41:02 -0800

    advanced 3

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -12,15 +12,15 @@ int main(void)
 	int count;
 	unsigned long previous = 0, current = 1, sum = 0;
 
-	for (count = 0; current <= 4000000; ++count)
+	for (count = 0; current < 4000000; ++count)
 	{
 		unsigned long temp;
 
 		temp = current;
 		current += previous;
 		previous = temp;
-		sum += current;
-		printf("%lu", current);
+		if (current < 4000000 && current % 2 == 0)
+			sum += current;
 	}
 	printf("%lu\n", sum);
 	return (0);
