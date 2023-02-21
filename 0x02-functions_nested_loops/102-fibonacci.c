a92d824c0ae0c0b305d4a87b698eb17f119e9131
muhammad.baheuddeen@gmail.com <baheuddeen>
2023-02-21 07:49:31 -0800

    advanced 2

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,18 +9,18 @@
 int main(void)
 {
 	int i;
-	unsigned long long previous, current;
+	unsigned long previous, current;
 
 	previous = 1;
 	current = 2;
-	printf("%lld, %lld, ", previous, current);
+	printf("%ld, %ld, ", previous, current);
 	for (i = 0; i < 50; i++)
 	{
 		int temp = current;
 
 		current += previous;
 		previous = temp;
-		printf("%lld", current);
+		printf("%ld", current);
 		if (i != 49)
 			printf(", ");
 	}
