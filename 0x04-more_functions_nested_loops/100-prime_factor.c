65e21c8bd011a2aefb1d3a54ba053cb31e15e724
muhammad.baheuddeen@gmail.com <baheuddeen>
2023-02-23 03:29:35 -0800

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -  Entry point
+ * Return: 0 success.
+ */
+int main(void)
+{
+	int largest_prime_factor, i = 1;
+	long number = 612852475143;
+
+	while ((number / i) > 0)
+	{
+		if ((number % i) == 0)
+		{
+			largest_prime_factor = i;
+			number /= i;
+		}
+		i++;
+	}
+	printf("%d\n", largest_prime_factor);
+	
+	return (0);
+}
