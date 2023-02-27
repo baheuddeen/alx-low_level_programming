8c048a1f92c55799d6ce442fbc6266f1944e5fd9
baheuddeen <muhammad.baheuddeen@gmail.com>
2023-02-27 14:56:45 +0200

    remove array syntax

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,20 +11,20 @@ int _atoi(char *s)
 	int i = 0, l = 0;
 	int result = 0, value = 1;
 
-	while (s[l] != '\0')
+	while (*(s + l) != '\0')
 	{
 		l++;
 	}
 	for (i = l; i > 0; i--)
 	{
-		int digit = s[i - 1] - '0';
+		int digit = *(s + i - 1) - '0';
 
 		if( digit >= 0 &&  digit <= 9)
 		{
 			result += value * digit;
 			value *= 10;
 		}
-		if(s[i - 2] == '-')
+		if(*(s + i - 2) == '-')
 			result *= -1;
 	}
 	printf("%d", result);
