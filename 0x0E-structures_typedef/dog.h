676812d4582f39459af3980f7dd99b3c47fcfa9c
baheuddeen <muhammad.baheuddeen@gmail.com>
2023-03-20 11:51:16 +0200

    make betty happy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog
+ * @name: dog name.
+ * @age: dog age.
+ * @owner: the owner of the dog.
+ */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
-
 
 #endif
