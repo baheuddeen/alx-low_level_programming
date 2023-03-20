0599cb5761beb2f27c42597feb76f4e5624e9f86
baheuddeen <muhammad.baheuddeen@gmail.com>
2023-03-20 12:02:04 +0200

    make betty happy ..

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,11 +6,10 @@
  * @name: dog name.
  * @age: dog age.
  * @owner: the dog owner.
-*/
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
-
