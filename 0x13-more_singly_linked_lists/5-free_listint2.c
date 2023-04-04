d0c599a304ee667694d875528c6ae9bbf4c0e0db
baheuddeen <muhammad.baheuddeen@gmail.com>
2023-04-05 00:03:56 +0200

    free 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list.
+ * @head: linked list head.
+ * Return: void.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current_node = (*head);
+
+	while (current_node != NULL)
+	{
+		*head = (*head)->next;
+		free(current_node);
+		current_node = *head;
+	}
+	*head = NULL;
+}
