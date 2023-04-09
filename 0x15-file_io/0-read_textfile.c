38864d996de6433bbca97423b63b2294275b0667
baheuddeen <muhammad.baheuddeen@gmail.com>
2023-04-09 22:22:39 +0200

    try

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,9 +13,10 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	size_t counter = 0, i = 0;
+	size_t counter = 0, i = 0, bytes_read;
 	FILE *file_to_read;
 	char buffer[BUF_SIZE];
+
 
 	if (!filename)
 		return (counter);
@@ -27,14 +28,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (counter);
 	}
 
-	while (!feof(file_to_read))
+	while (i < letters)
 	{
-		size_t bytes_read = fread(buffer, 1, BUF_SIZE, file_to_read);
-		
-		for (i = 0; counter < letters && i < bytes_read; i++)
-		{	
-			counter += write(STDOUT_FILENO, &buffer[i], 1);
-		}
+		bytes_read = fread(buffer, sizeof(char), BUF_SIZE, file_to_read);
+		if (bytes_read == 0)
+			break;
+		if (bytes_read < BUF_SIZE)
+			buffer[bytes_read] = '\0';
+		else
+			buffer[BUF_SIZE - 1] = '\0';
+		counter += bytes_read;
+		i += bytes_read;
+		printf("%s", buffer);
 	}
 
 	return (counter);
